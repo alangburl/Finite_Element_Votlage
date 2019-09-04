911e39d309eefe61d36772c766ff660cdba2eb0b
alangburl <39042458+alangburl@users.noreply.github.com>
2019-09-04 17:21:36 -0500

    Add files via upload

--- Calculation.py
+++ Calculation.py
@@ -27,12 +27,30 @@ class Voltage_Calculation():
     def boundary_conditions(self):
         '''Handle the boundary conditions'''
         #first extract the data from the information dictionary
+        x_locator=0
         for i in self.information.keys():
-            
+            #calling the function to find the node corresponding to the
+            #node value having a value than the starting point in of the given
+            #dictionary list for given direction
+            self.find_locations(self.information[i])
         
     def field_conditions(self):
         '''Handle the nodes in the field of the matrix'''
         
+    def find_locations(self,geometry_list):
+        '''Used to find the starting and ending indices of the matrix to
+        and set the locations to the specified potential in the A matrix'''
+        #first handle the possible terminals on the vertical sides
+        if geometry_list[0]==self.x_dim or geometry_list[0]==0 and geometry_list[2]==0:
+            #deterimine the starting point in the matrix going from top down
+            start_location=0
+            end_location=0
+            for j in range(len(self.y_nodes)):
+                if geometry_list[1]<=self.y_nodes[j]:
+                    start_location=j
+                elif geometry_list[1]+geometry_list[3]<=self.y_nodes[j]:
+                    end_location=j
+            
 if __name__=="__main__":
     Voltage_Calculation(100,100,100,100)
         
